91cf0cb62d83b352351fb2c1ffdb0195c6baab64
unknown <you@example.comd1sk@team4s.de>
2020-02-05 00:48:25 +0100

    start

---
+++ src/homepage.components.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import App from './App';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+
+      <div className='homepage'>
+      <div className='directory-menu'> 
+        <div className='menu-item'>
+            <div className='conenten'>
+                <h1 classname='ttile'>Hats</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+            </div>
+        </div>
+      </div>
+  
+    </div>
+)
+
+export default HomePage;
