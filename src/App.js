35748f352c921b7f0d9465f2dde7c9618ed3cb69
unknown <you@example.comd1sk@team4s.de>
2020-02-09 22:02:53 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.components';
-
+import ShopPage from './pages/shop/shop.components.jsx';
 
 function App() {
   return (
     <div>
-    <HomePage />
-    </div>
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop' component={ShopPage} />
+    </Switch>
+  </div>
   );
 }
 
