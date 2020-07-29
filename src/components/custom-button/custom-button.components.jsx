73e2531cce874a19ac5bbbecdc9725ff34341cf5
unknown <you@example.comd1sk@team4s.de>
2020-07-29 20:29:29 +0200

    cart icon and action acrt reducer add

--- src/components/custom-button/custom-button.components.jsx
+++ src/components/custom-button/custom-button.components.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './custom-button.styles.scss'
 
-const CustomButton = ({children, isGoogleSignIN, ...otherProps}) => (
+const CustomButton = ({children, isGoogleSignIN, inverted, ...otherProps}) => (
     
     <button 
-        className={`${isGoogleSignIN ? 'google-sign-in' : ''} custom-button`}
+        className={`${inverted ? 'inverted': ''} ${
+            isGoogleSignIN ? 'google-sign-in' : ''
+        } custom-button`}
         {...otherProps}
      >
     {children}
